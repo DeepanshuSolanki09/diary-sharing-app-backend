2bb85c486db5ec06269fe470cae5d168d3447152
Deepanshu solanki <solankideepanshu2006@gmail.com>
2025-07-01 19:14:26 +0530

    Update cloudninary.js

--- config/cloudninary.js
+++ config/cloudninary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-    cloud_name:"dqaxxy9k1",
-    api_key :"847697239817776" ,
-    api_secret:"7Gl4Hk5pHX17Oc6HxvL14mGm8Q8",
+    cloud_name:"",
+    api_key :"" ,
+    api_secret:"",
 })
 
 module.exports = cloudinary
